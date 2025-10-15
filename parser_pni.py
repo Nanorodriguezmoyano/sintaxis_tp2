e4c3d6c2531ed7547eceb3ea6d1aacb63eeb6acb
Nano <narodriguez@frd.utn.edu.ar>
2025-10-15 14:48:19 -0300

    Parser oraculoooooooooo

--- parser_pni.py
+++ parser_pni.py
@@ -1,4 +1,5 @@
 from producciones import *
+from tests import *
 
 VT=[TOKEN_OPERADOR_RELACIONAL,TOKEN_PR_PROGRAM,TOKEN_PR_VAR,TOKEN_PR_BEGIN,TOKEN_PR_END, TOKEN_PR_IF,TOKEN_PR_ELSE,TOKEN_PR_INT,TOKEN_PR_BOOL,TOKEN_PR_TRUE,TOKEN_PR_FALSE,TOKEN_PR_NOT,TOKEN_PR_AND,TOKEN_PR_OR,TOKEN_PR_GOTO,TOKEN_PR_LET,TOKEN_ASIGNACION,TOKEN_OM_MAS, TOKEN_OM_GUION, TOKEN_OM_ASTERISCO, TOKEN_SP_PUNTO,TOKEN_SP_COMA,TOKEN_SP_DOSPUNTOS, TOKEN_SP_PUNTOCOMA,TOKEN_SP_PARENTESIS_IZQ,TOKEN_SP_PARENTESIS_DER,TOKEN_SP_TRIPLEPUNTO,TOKEN_NUM,TOKEN_ID,'Eof']
 VN=[TCode, Body, DecVarList,DecVar,DecVarBody,Statement,StatementList,StatementBody,Goto,Assignment,Op,MatOp,BoolOp,Lvalue,Rvalue,Conditional,CompExpr,CompOp,DecVarList0,StatementList0,Rbody,A0,B0,C0,Bool0]
@@ -7,17 +8,17 @@ def desc_rec_proc(codigo_fuente):
     datos_locales= {
         'lista_tokens' : codigo_fuente,
         'error' : False,
-        'index': 0
+        'index': 0,
+        'derivaciones':[]
     }
     
-
     def pni(no_terminal):
         if datos_locales['lista_tokens'][datos_locales['index']] in P[no_terminal].keys():
-            produccion = P[no_terminal][datos_locales['lista_tokens'][datos_locales['index']]]
-            procesar(produccion)
-            
-
-            
+            cuerpo_produccion = P[no_terminal][datos_locales['lista_tokens'][datos_locales['index']]]
+            datos_locales['derivaciones'].append({no_terminal:cuerpo_produccion})
+            procesar(cuerpo_produccion)
+        else:
+            print(f'Error, no hay regla para {datos_locales['lista_tokens'][datos_locales['index']]}, con {no_terminal}')
 
     def procesar(cuerpo_produccion): 
         for simbolo in cuerpo_produccion:
@@ -25,13 +26,17 @@ def desc_rec_proc(codigo_fuente):
             datos_locales['error'] = False
             if simbolo in VT:
                 if simbolo == caracter_actual:
-                    datos_locales['index'] += 1                        
+                    datos_locales['index'] += 1    
+                                        
                 else:
                     datos_locales['error'] = True
+                    print(f'Error, se esperaba {simbolo}, y se encontro, {caracter_actual}')
+                    print(datos_locales['derivaciones'])
                     break
             elif simbolo in VN:
                 pni(simbolo)
                 if datos_locales['error']:
+                    
                     break
 
     def principal(): 
@@ -41,9 +46,10 @@ def desc_rec_proc(codigo_fuente):
             print('La cadena no pertenece al lenguaje')
             return False
         print('La cadena pertenece al lenguaje') 
+        print(datos_locales['derivaciones'])
         return True
-    
     return principal()
 
-programa=[TOKEN_PR_PROGRAM, TOKEN_SP_PUNTO, TOKEN_PR_BEGIN,TOKEN_PR_END,'Eof']
-desc_rec_proc(programa)
+programa=[TOKEN_PR_PROGRAM, TOKEN_SP_PUNTO, TOKEN_PR_BEGIN,TOKEN_PR_END,TOKEN_PR_END,'Eof']
+
+desc_rec_proc(tokens_validos)
---
+++ tests.py
@@ -0,0 +1,41 @@
+from tokens import *
+tokens_validos = [
+    TOKEN_PR_PROGRAM, TOKEN_SP_PUNTO,               # program .
+    TOKEN_PR_VAR,                                   # var
+    TOKEN_ID, TOKEN_SP_DOSPUNTOS,                   # x :
+    TOKEN_PR_INT, TOKEN_SP_PARENTESIS_IZQ, TOKEN_NUM, TOKEN_SP_TRIPLEPUNTO, TOKEN_NUM,
+    TOKEN_SP_PARENTESIS_DER, TOKEN_ASIGNACION, TOKEN_NUM, TOKEN_SP_PUNTOCOMA,  # int(0...10)=0;
+    TOKEN_PR_BEGIN,                                 # begin
+    TOKEN_PR_IF, TOKEN_ID, TOKEN_OPERADOR_RELACIONAL, TOKEN_NUM,               # if x < 10
+    TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,                             # goto label;
+    TOKEN_PR_ELSE, TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,              # else goto end;
+    TOKEN_PR_END,                                   # end
+    'Eof'
+]
+tokens_invalidos = [
+    TOKEN_PR_PROGRAM, TOKEN_SP_PUNTO,             # program .
+    TOKEN_PR_VAR,                                   # var
+    TOKEN_ID,                   # x :
+    TOKEN_PR_INT, TOKEN_SP_PARENTESIS_IZQ, TOKEN_NUM, TOKEN_SP_TRIPLEPUNTO, TOKEN_NUM,
+    TOKEN_SP_PARENTESIS_DER, TOKEN_ASIGNACION, TOKEN_NUM, TOKEN_SP_PUNTOCOMA,  # int(0...10)=0;
+    TOKEN_PR_BEGIN,                                 # begin
+    TOKEN_PR_IF, TOKEN_ID, TOKEN_OPERADOR_RELACIONAL, TOKEN_NUM,               # if x < 10
+    TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,                             # goto label;
+    TOKEN_PR_ELSE, TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,              # else goto end;
+    TOKEN_PR_END,                                   # end
+    'Eof'
+]
+
+tokens_invalidos2 = [
+    TOKEN_SP_PUNTO,         # program .
+                                 # var
+    TOKEN_ID, TOKEN_SP_DOSPUNTOS,                   # x :
+    TOKEN_PR_INT, TOKEN_SP_PARENTESIS_IZQ, TOKEN_NUM, TOKEN_SP_TRIPLEPUNTO, TOKEN_NUM,
+    TOKEN_SP_PARENTESIS_DER, TOKEN_ASIGNACION, TOKEN_NUM, TOKEN_SP_PUNTOCOMA,  # int(0...10)=0;
+    TOKEN_PR_BEGIN,                                 # begin
+    TOKEN_PR_IF, TOKEN_ID, TOKEN_OPERADOR_RELACIONAL, TOKEN_NUM,               # if x < 10
+    TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,                             # goto label;
+    TOKEN_PR_ELSE, TOKEN_PR_GOTO, TOKEN_ID, TOKEN_SP_PUNTOCOMA,              # else goto end;
+    TOKEN_PR_END,                                   # end
+    'Eof'
+]
